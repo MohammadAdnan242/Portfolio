5d3652b707739651d6f96b0fc24fedfabbde57ca
MohammadAdnan242 <adnan242029@gmail.com>
2025-02-13 19:44:33 +0530

    update profpic

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -70,12 +70,20 @@ const Hero = () => {
                 transition={{ duration: 1, delay: 1.5 }}
                 className="flex flex-row items-center gap-6 my-4 md:mb-0"
                 >
-                    <motion.button whileHover={{ scale: 1.05, boxShadow: "0px 0px 8px rgba(0, 0, 0, 0.3)" }}
-                        className="z-10 cursor-pointer font-bold text-gray-200 md:w-auto p-4 border
-                                     border-purple-400 rounded-xl" href="#"    
-                    >
-                        Download CV
-                    </motion.button>
+ <a 
+      href="https://drive.google.com/file/d/1QttxlOMVybKh59lNEJV7aRkAQ6ao6kva/view?usp=sharing" 
+      target="_blank" 
+      rel="noopener noreferrer"
+>
+    <motion.button 
+        whileHover={{ scale: 1.05, boxShadow: "0px 0px 8px rgba(0, 0, 0, 0.3)" }}
+        className="z-10 cursor-pointer font-bold text-gray-200 md:w-auto p-4 border
+                   border-purple-400 rounded-xl"
+    >
+        Download CV
+    </motion.button>
+</a>
+
 
                     <div className="flex gap-6 flex-row text-4xl md:text-6xl text-purple-400 z-20">
                         <motion.a whileHover={{ scale: 1.2 }} href="https://github.com/MohammadAdnan242">
